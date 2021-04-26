c19aaccfdac1166b4a7c489f3c33b48b68bacbac
nnnnusui <nnnnusui@gmail.com>
2021-04-26 15:06:40 +0900

    add: test.ts

---
+++ src/test.ts
@@ -0,0 +1,14 @@
+
+import { chainN } from "./main/chainN";
+import { Combinator } from "./main/type/Combinator";
+
+type Parser = Combinator<string, string>;
+const any: Parser = (context) => {
+  const [head, ...tails] = context;
+  console.log(tails);
+  return Combinator.ok(tails.join(""), head);
+};
+const test = chainN(any, any, any);
+console.dir(test("abcde"), {
+  depth: Number.MAX_VALUE,
+});
